1098f28eea1e46ba7b9ab1531049962bad87a82f
Mercede Isabel Benavides <Mercede Isabel Benavides>
2016-11-09 14:20:15 -0500

    dasads

--- application/views/venta/venta_crear_v.php
+++ application/views/venta/venta_crear_v.php
@@ -70,11 +70,63 @@
                 ?>
               </datalist>
             </div>
+            
 
+            <div class="col-md-12">
+              <h4>Detalle de venta</h4>
+              <hr>
+              <div>
+                <div id="form_detalle" >
+                
+                  <div class="form-group  col-md-6">
+                    <label>Nombre Producto</label>
+                    <input type="text" class="form-control" name="nombre_add_detalle"  placeholder="Nombre Producto">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label>Presentación</label>
+                    <input type="text" class="form-control" name="presentacion_add_detalle" placeholder="Presentación">
+                  
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label>Cantidad</label>
+            
+                    <input type="text" class="form-control" name="cantidad_add_detalle" placeholder="Cantidad">
+                  
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label>Precio</label>
+                    <input type="text" class="form-control" name="precio_add_detalle" placeholder="Precio">
+                  
+                  </div>
+               
+                  <div class="form-group col-md-12">
+                    <a href="" id="agregar_detalle" class="btn btn-primary">Agregar Detalle </a>
+                  </div>
+                 </div>
+                <div class="" id="table_detalle">
+                  <table class="table table_arreglar">
+                    <thead>
+                      <tr class="arreglar">
+                        <th>Nombre Producto</th>
+                        <th>Presentacion</th>
+                        <th>Precio Unitario</th>
+                        <th>Cantidad</th>
+                        <th>Cantidad Total</th>
+                        <th>Acciones</th>
+                      </tr>
+                    </thead>
+                    <tbody id="tbody_detalle">
+                    
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            
+            </div>
             <div class="col-md-12">
 
                 <input type="hidden" name="IdTrabajador" value="2">
-              <button type="submit" class="btn btn-primary ">Agregar</button>
+                <button type="submit" class="btn btn-primary ">Agregar</button>
             
             </div>
           </form>
@@ -85,6 +137,32 @@
 </div>
 </div>
 </div>
-
+<script>
+  var $form_detalle = document.getElementById("form_detalle");
+  var $tbody_detalle = document.getElementById("tbody_detalle");
+  console.log($form_detalle);
+  $form_detalle.onclick = function(e){
+      e.preventDefault();
+      
+      if(e.target.id == "agregar_detalle"){
+        let add_detalle = {
+            nombre : $form_detalle.getElementsByTagName("input")[0].value,
+            presentacion : $form_detalle.getElementsByTagName("input")[1].value,
+            cantidad : $form_detalle.getElementsByTagName("input")[2].value,
+            precio : $form_detalle.getElementsByTagName("input")[3].value
+          };
+         $tbody_detalle.innerHTML += `<tr>
+          <td> <input value=${add_detalle.nombre} name="detalle_nombre[]" >  </input> </td>
+          <td>  <input value=${add_detalle.presentacion} name="detalle_presentacion[]" >   </input> </td>
+          <td>  <input value=${add_detalle.cantidad}  name="detalle_cantidad[] " ></input> </td>
+          <td> <input value=${add_detalle.precio} name="detalle_precio[] ">   </input> </td>
+          <td> total </td>
+          <td> acciones </td>
+         </tr>`;
+       
+        
+      }
+  }
+</script>
 <?php  $this->load->view('layouts/footer.php');?>       
      
